6a57952d1a10c371778d2c41b128d19cf0ab644f
Trevor Puccini <trevorpuccini@comcast.net>
2020-03-05 09:49:39 -0800

    Define getElementsByClassName function
    Define function without using native document.getElementsByClassName.
    Create inner function that checks each HTML element for input class
    to 'getElementsByClassName', and adds to an array. If element has child
    nodes, iterate through child nodes and call inner 'checkForClass' function
    recursively.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,49 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
-  // your code here
+    var elements = [];
+    var body = document.body;
+
+    var checkForClass = function(element) {
+        if (element.classList && element.classList.contains(className)) {
+            elements.push(element);
+        }
+        if (element.childNodes) {
+            for (var i = 0; i < element.childNodes.length; i++) {
+                checkForClass(element.childNodes[i]);
+            }
+        }
+
+    }
+    checkForClass(body);
+    console.log(elements[0]);
+    return elements;
 };
+
+// <body>
+//     <div class="targetClassName"></div>
+//     <div class="otherClassName targetClassName"></div>
+//     <div>
+//         <div class="targetClassName"></div>
+//     </div>
+//     <div>
+//         <div class="targetClassName">
+//             <div class="targetClassName"></div>
+//         </div>
+//     </div>
+//     <div>
+//         <div></div>
+//         <div>
+//             <div class="targetClassName"></div>
+//         </div>
+//     </div>
+//     <div>
+//         <div class="targetClassName"></div>
+//         <div class="targetClassName"></div>
+//     </div>
+//     <div>
+//         <div class="somediv">
+//             <div class="innerdiv"><span class="targetClassName">yay</span></div>
+//         </div>
+//     </div>
+// </body>
